08d424a8a2ba0eddacf55c7a277db2a44f911414
AnelejB <banjac.jelena8@gmail.com>
2021-01-10 12:13:32 +0100

    Add /blog and Markdown support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,16 @@
  */
 
   /* Your site config here */
-  module.exports = {
-    plugins: [`gatsby-plugin-netlify-cms`],
-  }
-
+module.exports = {
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`
+  ]
+}
